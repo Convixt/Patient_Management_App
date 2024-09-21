c99ffff06503b86e71a7baecae2b1fed26239c1e
NishithPandey <pnishith215@gmail.com>
2024-09-22 02:03:22 +0530

    updated data table of admin panel

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,95 +1,14 @@
 import StatCard from "@/components/StatCard";
-import {columns, Payment} from "@/components/table/columns";
-import {DataTable} from "@/components/table/DataTable";
+import { columns, Payment } from "@/components/table/columns";
+import { DataTable } from "@/components/table/DataTable";
 import { getRecentAppointmentList } from "@/lib/actions/appointment.action";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-
-async function getData(): Promise<Payment[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    // ...
-  ]
-}
-
 const Admin = async () => {
+  const appointments = await getRecentAppointmentList();
 
-  const appointments = await getRecentAppointmentList()
-  const data = await getData()
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
       <header className="admin-header">
@@ -108,37 +27,33 @@ const Admin = async () => {
       <main className="admin-main">
         <section className="w-full space-y-4">
           <h1>Welcome 👋</h1>
-          <p className="text-dark-700">Start the day with managing new appointments</p>
+          <p className="text-dark-700">
+            Start the day with managing new appointments
+          </p>
         </section>
 
         <section className="admin-stat">
-          <StatCard 
-          type="appointments"
-          count={appointments.scheduledCount}
-          label="Scheduled appointments"
-          icon = "/assets/icons/appointments.svg"
-          
+          <StatCard
+            type="appointments"
+            count={appointments.scheduledCount}
+            label="Scheduled appointments"
+            icon="/assets/icons/appointments.svg"
           />
-          <StatCard 
-          type="pending"
-          count={appointments.pendingCount}
-          label="Pending appointments"
-          icon = "/assets/icons/pending.svg"
-          
+          <StatCard
+            type="pending"
+            count={appointments.pendingCount}
+            label="Pending appointments"
+            icon="/assets/icons/pending.svg"
           />
-          <StatCard 
-          type="cancelled"
-          count={appointments.cancelledCount}
-          label="Cancelled appointments"
-          icon = "/assets/icons/cancelled.svg"
-          
+          <StatCard
+            type="cancelled"
+            count={appointments.cancelledCount}
+            label="Cancelled appointments"
+            icon="/assets/icons/cancelled.svg"
           />
-          
         </section>
 
-        <DataTable columns = {columns} data={data}  />
-        {/* <DataTable columns = {columns} data={appointments.data}  /> */}
-
+        <DataTable columns={columns} data={appointments.documents} />
       </main>
     </div>
   );
